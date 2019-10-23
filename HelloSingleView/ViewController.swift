442180fc854d34284b157c3a3270194e479a9f50
Adam Sturesson <adam99.sturesson@hotmail.se>
2019-10-23 13:29:59 +0200

    Initial Commit

---
+++ HelloSingleView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloSingleView
+//
+//  Created by Adam Sturesson on 2019-10-23.
+//  Copyright © 2019 Adam Sturesson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
